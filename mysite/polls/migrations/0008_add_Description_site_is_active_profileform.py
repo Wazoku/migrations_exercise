a9d6d1cef207d86112f92760a77de13eab231e3a
Diego <diego.g0912@hotmail.es>
2023-09-27 18:21:07 -0500

    stage 1,one migration

--- mysite/polls/migrations/0008_add_Description_site_is_active_profileform.py
+++ mysite/polls/migrations/0008_add_Description_site_is_active_profileform.py
@@ -5,9 +5,17 @@ class Migration(migrations.Migration):
 
     dependencies = [
         ('polls', '0007_alter_answer_poll'),
-                ]
+    ]
 
     operations = [
+        migrations.AddField(
+            model_name='profileform',
+            name='is_active',
+            field=models.BooleanField(
+                default=True,
+                null=True
+                )
+        ),
         migrations.AddField(
             model_name="site",
             name="description",
