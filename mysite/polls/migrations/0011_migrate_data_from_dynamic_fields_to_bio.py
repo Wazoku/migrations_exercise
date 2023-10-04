5d694b9b1c06c30d1a75931dc292dc70dcc390de
Oluwatosin Olubiyi <olubiyiontheweb@gmail.com>
2023-10-04 16:39:37 +0100

    data migrated from dynamic fields to bio

---
+++ mysite/polls/migrations/0011_migrate_data_from_dynamic_fields_to_bio.py
@@ -0,0 +1,21 @@
+from django import apps
+from django.db import migrations
+from django.db.models import F
+
+
+def copy_field(apps, schema):
+    model = apps.get_model('polls', 'Profile')
+    model.objects.all().update(bio=F('dynamic_fields'))
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ('polls', '0010_profileform_is_active'),
+    ]
+
+    operations = [
+        migrations.RunPython(
+            code=copy_field,
+            reverse_code=migrations.RunPython.noop,
+        ),
+    ]
